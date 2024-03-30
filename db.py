8465dd100af331c88befe9eb3a2eea9e53d31eee
Rafal Pietras <rafal.radx@gmail.com>
2024-03-30 23:48:38 +0100

    secretes hidden in env vars

--- db.py
+++ db.py
@@ -1,10 +1,13 @@
 from sqlalchemy import create_engine, Column, Integer, String, JSON, Date
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URL = (
-    "postgresql+psycopg2://postgres:567234@localhost:5432/postgres"
-)
+load_dotenv()
+
+SQLALCHEMY_DATABASE_URL = os.environ.get("SQLALCHEMY_DATABASE_URL")
+
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
