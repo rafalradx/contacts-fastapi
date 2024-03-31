c277ead802c2090218e37e778741403c1b46575e
Rafal Pietras <rafal.radx@gmail.com>
2024-04-01 01:55:48 +0200

    alembic set up

--- src/database/db.py
+++ src/database/db.py
@@ -1,13 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-import os
-from dotenv import load_dotenv
 from src.config import settings
 
-load_dotenv()
-
-
-SQLALCHEMY_DATABASE_URL = os.environ.get("SQLALCHEMY_DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = settings.sqlalchemy_database_url
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
