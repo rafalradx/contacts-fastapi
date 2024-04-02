04435d5320ea5bc7e64cf16173a34b19801f256b
Rafal Pietras <rafal.radx@gmail.com>
2024-04-03 00:46:17 +0200

    users schema added

--- src/database/models.py
+++ src/database/models.py
@@ -1,4 +1,7 @@
-from sqlalchemy import Column, Integer, String, JSON, Date
+from sqlalchemy import Column, Integer, String, JSON, Date, Boolean, func
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql.schema import ForeignKey
+from sqlalchemy.sql.sqltypes import DateTime
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -20,3 +23,19 @@ class Contact(MyBase):
     phone_number = Column(String(15))
     birth_date = Column(Date)
     additional_data = Column(JSON, nullable=True)
+    user_id = Column(
+        "user_id", ForeignKey("users.id", ondelete="CASCADE"), default=None
+    )
+    user = relationship("User", backref="contacts")
+
+
+class User(MyBase):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50))
+    email = Column(String(250), nullable=False, unique=True)
+    confirmed = Column(Boolean, default=False)
+    password = Column(String(255), nullable=False)
+    created_at = Column("crated_at", DateTime, default=func.now())
+    avatar = Column(String(255), nullable=True)
+    refresh_token = Column(String(255), nullable=True)
---
+++ src/schemas/users.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+from pydantic import BaseModel, Field, EmailStr
+
+
+class UserIn(BaseModel):
+    username: str = Field(min_length=5, max_length=16)
+    email: EmailStr
+    password: str = Field(min_length=6, max_length=10)
+
+
+class UserOut(BaseModel):
+    id: int
+    username: str
+    email: EmailStr
+    created_at: datetime
+    avatar: str
+
+
+class UserCreated(BaseModel):
+    user: UserOut
+    detail: str = "User successfully created"
